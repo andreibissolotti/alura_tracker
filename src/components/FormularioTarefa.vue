773905db236b5b6810c2dcfef0b045642479e8cd
Andrei Bissolotti <andreibisso@gmail.com>
2022-12-23 15:35:10 -0300

    Extrai toda a logica do temporizador para um template

--- src/components/FormularioTarefa.vue
+++ src/components/FormularioTarefa.vue
@@ -5,21 +5,7 @@
         <input type="text" class="input" placeholder="Qual tarefa você deseja iniciar" />
       </div>
       <div class="column">
-        <div class="is-flex is-align-items-center is-justify-content-space-between">
-          <CronometroFormatado :tempoEmSegundos='tempoEmSegundos' />
-          <button class="button" @click="iniciar">
-            <span class="icon">
-              <i class="fas fa-play"></i>
-            </span>
-            <span>play</span>
-          </button>
-          <button class="button" @click="finalizar">
-            <span class="icon">
-              <i class="fas fa-stop"></i>
-            </span>
-            <span>stop</span>
-          </button>
-        </div>
+        <TeomporizadorTemplate />
       </div>
     </div>
   </div>
@@ -27,28 +13,12 @@
 
 <script lang="ts">
 import { defineComponent } from 'vue';
-import CronometroFormatado from './CronometroFormatado.vue'
+import TeomporizadorTemplate from './TemporizadorTemplate.vue'
 
 export default defineComponent({
   name: 'FormularioTarefa',
   components: {
-    CronometroFormatado
-  },
-  data() {
-    return {
-      tempoEmSegundos: 0,
-      cronometro: 0
-    }
-  },
-  methods: {
-    iniciar() {
-      this.cronometro = setInterval(() => {
-        this.tempoEmSegundos++
-      }, 1000)
-    },
-    finalizar() {
-      clearInterval(this.cronometro)
-    }
+    TeomporizadorTemplate
   }
 })
 </script>
