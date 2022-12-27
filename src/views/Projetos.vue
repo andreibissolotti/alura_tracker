2098a3cf9db30c8952dcfc39fa9675f987a03f7a
Andrei Bissolotti <andreibisso@gmail.com>
2022-12-26 22:48:22 -0300

    Cria a lista de projetos na aba projeto

--- src/views/Projetos.vue
+++ src/views/Projetos.vue
@@ -1,11 +1,64 @@
 <template>
-  <H1>Opaaa</H1>
+  <section class="projetos">
+    <H1 class="title">Projetos</H1>
+    <form @submit.prevent="salvar">
+      <div class="field">
+        <label for="nomeDoProjeto" class="label">
+          Nome do Projeto
+        </label>
+        <input class="input" type="text" v-model="nomeDoProjeto" id="nomeDoProjeto" />
+      </div>
+      <div class="field">
+        <button class="button" type="submit">
+          Salvar
+        </button>
+      </div>
+    </form>
+
+    <table class="table is-fullwidth">
+      <thead>
+        <tr>
+          <th>ID</th>
+          <th>Nome</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="projeto in projetos" :key="projeto.id">
+          <td>{{ projeto.id }}</td>
+          <td>{{ projeto.name }}</td>
+        </tr>
+      </tbody>
+    </table>
+  </section>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue';
+import IProjeto from '../interfaces/IProjeto'
 
 export default defineComponent({
-  name: 'ProjetosView'
+  name: 'ProjetosView',
+  data() {
+    return {
+      nomeDoProjeto: '',
+      projetos: [] as IProjeto[]
+    }
+  },
+  methods: {
+    salvar() {
+      const projeto: IProjeto = {
+        name: this.nomeDoProjeto,
+        id: new Date().toISOString()
+      }
+      this.projetos.push(projeto)
+      this.nomeDoProjeto = ''
+    }
+  }
 })
 </script>
+
+<style scoped>
+.projetos {
+  padding: 1.25rem;
+}
+</style>
