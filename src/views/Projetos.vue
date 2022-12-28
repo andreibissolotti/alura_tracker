9272bcaad9672aefcab74326fe48474ca653587e
Andrei Bissolotti <andreibisso@gmail.com>
2022-12-27 21:16:21 -0300

    Separa o formulario para criação e edição dos projetos da tabela de visualização

--- src/views/Projetos.vue
+++ src/views/Projetos.vue
@@ -1,31 +1,31 @@
 <template>
   <section class="projetos">
     <H1 class="title">Projetos</H1>
-    <form @submit.prevent="salvar">
-      <div class="field">
-        <label for="nomeDoProjeto" class="label">
-          Nome do Projeto
-        </label>
-        <input class="input" type="text" v-model="nomeDoProjeto" id="nomeDoProjeto" />
-      </div>
-      <div class="field">
-        <button class="button" type="submit">
-          Salvar
-        </button>
-      </div>
-    </form>
-
+    <RouterLink to="/projetos/novo" class="button">
+      <span class="icon is-small">
+        <i class="fas fa-plus"></i>
+      </span>
+      <span>Novo Projeto</span>
+    </RouterLink>
     <table class="table is-fullwidth">
       <thead>
         <tr>
           <th>ID</th>
           <th>Nome</th>
+          <th>Ações</th>
         </tr>
       </thead>
       <tbody>
         <tr v-for="projeto in projetos" :key="projeto.id">
           <td>{{ projeto.id }}</td>
           <td>{{ projeto.name }}</td>
+          <td>
+            <RouterLink :to="`/projetos/${projeto.id}/edit`" class="button">
+              <span class="icon is-small">
+                <i class="fas fa-pencil-alt"></i>
+              </span>
+            </RouterLink>
+          </td>
         </tr>
       </tbody>
     </table>
@@ -38,21 +38,9 @@ import { useStore } from '@/store';
 
 export default defineComponent({
   name: 'ProjetosView',
-  data() {
-    return {
-      nomeDoProjeto: ''
-    }
-  },
-  methods: {
-    salvar() {
-      this.store.commit('ADICIONA_PROJETO', this.nomeDoProjeto)
-      this.nomeDoProjeto = ''
-    }
-  },
   setup() {
     const store = useStore()
     return {
-      store,
       projetos: computed(() => store.state.projetos)
     }
   }
---
+++ src/views/projetos/FormularioProjetos.vue
@@ -0,0 +1,51 @@
+<template>
+  <section class="projetos">
+    <H1 class="title">Projetos</H1>
+    <form @submit.prevent="salvar">
+      <div class="field">
+        <label for="nomeDoProjeto" class="label">
+          Nome do Projeto
+        </label>
+        <input class="input" type="text" v-model="nomeDoProjeto" id="nomeDoProjeto" />
+      </div>
+      <div class="field">
+        <button class="button" type="submit">
+          Salvar
+        </button>
+      </div>
+    </form>
+  </section>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+import { useStore } from '@/store';
+
+export default defineComponent({
+  name: 'FormularioProjetos',
+  data() {
+    return {
+      nomeDoProjeto: ''
+    }
+  },
+  methods: {
+    salvar() {
+      this.store.commit('ADICIONA_PROJETO', this.nomeDoProjeto)
+      this.nomeDoProjeto = ''
+      this.$router.push('/projetos')
+    }
+  },
+  setup() {
+    const store = useStore()
+    return {
+      store
+    }
+  }
+})
+</script>
+
+<style scoped>
+.projetos {
+  padding: 1.25rem;
+}
+</style>
